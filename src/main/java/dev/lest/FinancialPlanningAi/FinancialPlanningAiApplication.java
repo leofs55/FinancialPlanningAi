3138faad089e610205e7541f0347d8a3d38dc67b
Leonardo Figorelli Santana <leofigorelli@gmail.com>
2025-09-22 16:19:22 -0300

    first commit

---
+++ src/main/java/dev/lest/FinancialPlanningAi/FinancialPlanningAiApplication.java
@@ -0,0 +1,13 @@
+package dev.lest.FinancialPlanningAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialPlanningAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialPlanningAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lest/FinancialPlanningAi/FinancialPlanningAiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lest.FinancialPlanningAi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialPlanningAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
